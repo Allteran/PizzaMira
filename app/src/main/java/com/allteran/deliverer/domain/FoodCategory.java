acc69bfdb4dbecd83e44dcc30ad06ffb6f276cda
allteran <vitalii.prozapas@gmail.com>
2021-03-11 18:41:12 +0300

    Initial commit

---
+++ app/src/main/java/com/allteran/deliverer/domain/FoodCategory.java
@@ -0,0 +1,25 @@
+package com.allteran.deliverer.domain;
+
+public class FoodCategory {
+    private String id;
+    private String name;
+
+    public FoodCategory() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/allteran/deliverer/domain/FoodItem.java
@@ -0,0 +1,52 @@
+package com.allteran.deliverer.domain;
+
+public class FoodItem {
+    private String id;
+    private String name;
+    private String description;
+    private int price;
+    private FoodCategory category;
+
+    public FoodItem() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public FoodCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(FoodCategory category) {
+        this.category = category;
+    }
+}
